981702bec6e6ba5058775605a2fff89478a805da
thunderbiscuit <thunderbiscuit@protonmail.com>
2023-05-15 13:29:23 -0400

    Use Long instead of Int in splitAmount utility method

--- lib/src/main/kotlin/me/tb/Utilities.kt
+++ lib/src/main/kotlin/me/tb/Utilities.kt
@@ -4,18 +4,20 @@ import kotlin.math.pow
 
 /**
  * Given a total amount, returns the shortest list of token values to create this total, e.g. 13 is [1, 4, 8].
- * TODO: Is Int the correct type here? The maximum Int number is 2147483647, i.e. 0.02147483647 bitcoin.
- *       Is there a maximum amount for any given token? Is that a mint policy?
+ *
+ * TODO: After seeing that the default mint on the Python cashu implementation returns
+ *       keys for 64 bit integers I'm choosing Long as the type here, but keep in mind that because the amounts
+ *       are a mint policy, this library will need more resilience against mints that support different token amounts.
  */
-public fun splitAmount(value: Int): List<Int> {
-    require(value != 0) { "Zero amounts do not make sense in this context." }
-    require(value < 0) { "Negative amounts do not make sense in this context." }
+public fun splitAmount(value: Long): List<Long> {
+    require(value != 0L) { "Zero amounts do not make sense in this context." }
+    require(value > 0L) { "Negative amounts do not make sense in this context." }
 
-    val chunks: MutableList<Int> = mutableListOf()
-    for (i in 0 until 32) {
-        val mask: Int = 1 shl i
-        if ((value and mask) != 0) {
-            chunks.add(2.0.pow(i).toInt())
+    val chunks: MutableList<Long> = mutableListOf()
+    for (i: Int in 0 until 32) {
+        val mask: Long = 1L shl i
+        if ((value and mask) != 0L) {
+            chunks.add(2.0.pow(i).toLong())
         }
     }
     return chunks
--- lib/src/test/kotlin/me/tb/UtilitiesTest.kt
+++ lib/src/test/kotlin/me/tb/UtilitiesTest.kt
@@ -8,25 +8,26 @@ import kotlin.test.assertFailsWith
 class UtilitiesTest {
     @Test
     fun `splitAmount correctly splits small amounts`() {
-        assertEquals(splitAmount(13), listOf(1, 4, 8))
+        assertEquals<List<Long>>(splitAmount(13L), listOf(1, 4, 8))
     }
 
+    // Long.MAX_VALUE = 9223372036854775807L
     @Test
     fun `splitAmount correctly splits large amounts`() {
-        assertEquals(splitAmount(2147483647), listOf(1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824))
+        assertEquals<List<Long>>(splitAmount(9223372036854775807L), listOf(1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824, 2147483648))
     }
 
     @Test
     fun `splitAmount throws an exception on 0 amount values`() {
         assertFailsWith<IllegalArgumentException> {
-            splitAmount(0)
+            splitAmount(0L)
         }
     }
 
     @Test
     fun `splitAmount throws an exception on negative amount values`() {
         assertFailsWith<IllegalArgumentException> {
-            splitAmount(-1)
+            splitAmount(-1L)
         }
     }
 }
