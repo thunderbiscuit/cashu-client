75539a215f805123d9f4731939e00763d2ac9a47
thunderbiscuit <thunderbiscuit@protonmail.com>
2023-06-03 16:23:48 -0400

    Add MintRequest type

--- lib/src/main/kotlin/me/tb/BlindedMessage.kt
+++ lib/src/main/kotlin/me/tb/BlindedMessage.kt
@@ -2,8 +2,29 @@ package me.tb
 
 import kotlinx.serialization.Serializable
 
+/**
+ * The smallest unit of request to the mint endpoint.
+ *
+ * @param amount The amount of the token.
+ * @param blindedSecret The blinded secret, referred to as B_ in the spec (NUT-00).
+ */
 @Serializable
 public data class BlindedMessage(
     val amount: Long,
     val blindedSecret: String
+)
+
+/**
+ * This is the object sent to the mint endpoint and consists of a list of blinded messages to sign.
+ *
+ * NOTE: These are called PostMintRequest in the spec (NUT-04). The reason why I changed the name is because
+ *       PostMintRequest describes the object in terms of _when_ it is sent (after the "mint request" described in
+ *       NUT-03 has been sent) rather than what it is. `MintRequest` is, IMO, a better name.
+ *       See cashu-rs which also chose that name.
+ *
+ * @param blindedMessages List of blinded messages to be signed.
+ */
+@Serializable
+public data class MintRequest(
+    val blindedMessages: List<BlindedMessage>,
 )
