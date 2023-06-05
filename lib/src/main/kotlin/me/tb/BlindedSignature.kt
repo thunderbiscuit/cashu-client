461a0ac4e7c9c0b28d2ea65d518dacddf16d02ac
thunderbiscuit <thunderbiscuit@protonmail.com>
2023-06-05 12:30:40 -0400

    Refactor MintResponse type

--- lib/src/main/kotlin/me/tb/BlindedSignature.kt
+++ lib/src/main/kotlin/me/tb/BlindedSignature.kt
@@ -1,18 +1,30 @@
 package me.tb
 
 import kotlinx.serialization.Serializable
+import fr.acinq.bitcoin.PublicKey
 
+/**
+ * This is the smallest unit of data returned by the mint endpoint. These aggregate into a [BlindedSignatures] object.
+ *
+ * @param amount The value of the token.
+ * @param blindedKey The blinded signature, a point on the curve referred to as C_ in the spec (NUT-00).
+ * @param id The id of the keyset of the mint public keys that signed the token.
+ */
 @Serializable
 public data class BlindedSignature(
     val amount: Long,
     val blindedKey: String,
     val id: Long
-)
+) {
+    init {
+        require(PublicKey.fromHex(blindedKey).isValid()) { "Invalid blinded key: $blindedKey" }
+    }
+}
 
 /**
  * This is the object returned by the mint endpoint and consists of a list of blinded signatures.
  */
 @Serializable
-public data class BlindedSignatures(
-    val blindedSignatures: List<BlindedSignature>
+public data class MintResponse(
+    val promises: List<BlindedSignature>
 )
