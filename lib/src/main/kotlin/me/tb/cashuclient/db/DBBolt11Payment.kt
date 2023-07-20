91f45ed75920909fefad1458bf6b265e7bd6bb40
thunderbiscuit <thunderbiscuit@protonmail.com>
2023-07-19 21:17:05 -0400

    Add getAmountByHash method to retrieve payment amounts for specific payment requests

--- lib/src/main/kotlin/me/tb/cashuclient/db/DBBolt11Payment.kt
+++ lib/src/main/kotlin/me/tb/cashuclient/db/DBBolt11Payment.kt
@@ -6,7 +6,10 @@
 package me.tb.cashuclient.db
 
 import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
 
 /**
  * A table to store payment requests made by the mint. Once paid, these will later be used to request tokens.
@@ -17,4 +20,25 @@ public object DBBolt11Payment : Table() {
     // The hash here is really just a secret payment ID used to identify the payment between the mint and the client.
     public val hash: Column<String> = varchar("hash", 100)
     public val value: Column<ULong> = ulong("value")
+}
+
+/**
+ * Using a hash as the identifier for an amount requested and invoice paid, return said amount.
+ *
+ * @param hash The hash, used to identify the payment between the mint and the client.
+ */
+public fun getAmountByHash(hash: String): ULong {
+    DBSettings.db
+    // Database.connect("jdbc:sqlite:./cashu.sqlite3", "org.sqlite.JDBC")
+    return transaction {
+        SchemaUtils.create(DBBolt11Payment)
+
+        val amount = DBBolt11Payment.select {
+            DBBolt11Payment.hash eq hash
+        }.singleOrNull()
+
+        amount?.let {
+            it[DBBolt11Payment.value]
+        } ?: throw Exception("No amount found for hash $hash")
+    }
 }
