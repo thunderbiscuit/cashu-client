24da3d6de107253beedefd6c0d57315fdad0dd8f
thunderbiscuit <thunderbiscuit@protonmail.com>
2023-12-27 13:16:54 -0500

    Update license file type

--- lib/src/main/kotlin/me/tb/cashuclient/Constants.kt
+++ lib/src/main/kotlin/me/tb/cashuclient/Constants.kt
@@ -1,6 +1,6 @@
 /*
  * Copyright 2023 thunderbiscuit and contributors.
- * Use of this source code is governed by the Apache 2.0 license that can be found in the ./LICENSE file.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the ./LICENSE.txt file.
  */
  
 package me.tb.cashuclient
