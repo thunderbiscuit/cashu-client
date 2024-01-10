71341d1caaf97db99f71cc6c5c886a3e6944d47c
thunderbiscuit <thunderbiscuit@protonmail.com>
2024-01-09 21:05:25 -0500

    Combine melt quote messages into single file

--- lib/src/main/kotlin/me/tb/cashuclient/melt/MeltQuoteMessages.kt
+++ lib/src/main/kotlin/me/tb/cashuclient/melt/MeltQuoteMessages.kt
@@ -5,8 +5,24 @@
 
 package me.tb.cashuclient.melt
 
+import fr.acinq.lightning.payment.PaymentRequest
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import me.tb.cashuclient.types.EcashUnit
+import me.tb.cashuclient.types.PaymentRequestSerializer
+
+/**
+ * In order to proceed with a melt, Alice must request a quote for a given bolt11 invoice she would like paid. This is
+ * the object sent to the mint for this purpose.
+ *
+ * @property request The bolt11 Lightning invoice to be paid.
+ * @property unit    The unit the wallet would like to pay with.
+ */
+@Serializable
+public data class MeltQuoteRequest(
+    @Serializable(with = PaymentRequestSerializer::class) public val request: PaymentRequest,
+    public val unit: EcashUnit = EcashUnit.SAT,
+)
 
 // TODO: Look at making the expiry timestamp property typesafe.
 
